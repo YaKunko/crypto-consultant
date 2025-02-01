92dcc11ed91bcc24473ff542df7ba978a9659f80
Yaroslav <kucherenko.yarik@yahoo.com>
2025-02-02 01:13:49 +0200

    Updated project colors and add main section

---
+++ src/components/MainSection.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="row flex-center main-section">
+    <div class="col-6 column main-section-content">
+      <div class="main-section-content-title">Legal Crypto Consultant</div>
+      <div class="main-section-content-description">
+        Your AI consultant in the world of crypto regulation and taxation.
+      </div>
+    </div>
+    <div class="col-6 main-section-content-image">
+      <img
+        src="/images/crypto-consult.png"
+        alt="Main Section Image"
+        style="max-width: 100%; width: 30rem"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MainSection',
+
+  data() {
+    return {
+      clickCount: 0,
+    }
+  },
+})
+</script>
+
+<style lang="scss">
+.main-section {
+  .main-section-content {
+    .main-section-content-title {
+      font-size: 3rem;
+      line-height: 3rem;
+      font-weight: 500;
+      letter-spacing: -0.06rem;
+      color: #000;
+    }
+    .main-section-content-description {
+      margin-top: 1.5rem;
+      font-size: 1.5rem;
+      line-height: 2rem;
+      font-weight: 500;
+      color: rgba(0, 0, 0, 0.6);
+    }
+  }
+}
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,43 +1,14 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+  <q-layout class="main-layout" view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar class="row no-wrap justify-between items-center">
+        <div class="row no-wrap flex-center header-logo">
+          <img src="/images/logo.png" style="margin-right: 0.5rem; width: 2.375rem" />
+          LCC
+        </div>
+        <div class="flex flex-center launch-bot-btn">Launch</div>
       </q-toolbar>
     </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -46,72 +17,53 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import EssentialLink, { type EssentialLinkProps } from 'components/EssentialLink.vue';
-
-const linksList: EssentialLinkProps[] = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'MainLayout',
 
-  components: {
-    EssentialLink
-  },
+  components: {},
 
-  data () {
+  data() {
     return {
-      linksList,
-      leftDrawerOpen: false
+      leftDrawerOpen: false,
     }
   },
 
   methods: {
-    toggleLeftDrawer () {
-      this.leftDrawerOpen = !this.leftDrawerOpen;
+    toggleLeftDrawer() {
+      this.leftDrawerOpen = !this.leftDrawerOpen
+    },
+  },
+})
+</script>
+
+<style lang="scss">
+.main-layout {
+  .q-toolbar {
+    padding: 0.875rem 10%;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+    background-color: #fff;
+    @media (max-width: 768px) {
+      padding: 0.875rem 0.5rem;
+    }
+    .header-logo {
+      font-family: 'Kanit', sans-serif;
+      font-size: 2rem;
+      font-weight: 300;
+      color: #000;
+    }
+    .launch-bot-btn {
+      color: #fff;
+      padding: 0.875rem 1.875rem;
+      background-color: #000;
+      border-radius: 0.625rem;
+      font-size: 0.875rem;
+      font-weight: 600;
+      line-height: 1.25rem;
+      height: 3rem;
+      cursor: pointer;
     }
   }
-});
-</script>
+}
+</style>
